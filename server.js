ff9943daf9fbdf32fa727940b7ff6cca9dfab35f
yogesh1006 <yogeshkasliwal707@gmail.com>
2023-08-31 23:25:16 +0530

    jwt token generate for registration

--- server.js
+++ server.js
@@ -1,32 +1,21 @@
 const express = require("express");
 const { chats } = require("./data/data");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const colors = require("colors");
+const userRoutes = require("./routes/userRoutes");
 
-const app = express();
 dotenv.config();
+connectDB();
+const app = express();
+
+app.use(express.json()); // to accept JSON data
 
 app.get("/", (req, res) => {
   res.send("API is running successfully...");
 });
 
-app.get("/api/chats", (req, res) => {
-  res.send(chats);
-});
-
-app.get("/api/chat/:id", (req, res) => {
-  const { id } = req.params;
-  const result = chats.find((item) => {
-    if (item._id === id) {
-      return item;
-    } else {
-      return `No Data Found ${id}`;
-    }
-  });
-  res.json({
-    data: result,
-    message: "Data received successfully.",
-  });
-});
+app.use("/api/user", userRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(5000, console.log(`Server running on PORT ${PORT}...`));
+app.listen(5000, console.log(`Server running on PORT ${PORT}...`.yellow));
