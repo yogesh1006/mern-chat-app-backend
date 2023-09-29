580e87be15e68adf54478d770afb30f778a82a17
yogesh1006 <yogeshkasliwal707@gmail.com>
2023-09-29 23:03:44 +0530

    added id param

--- config/generateToken.js
+++ config/generateToken.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-const generateToken = () => {
+const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: "30d",
   });
